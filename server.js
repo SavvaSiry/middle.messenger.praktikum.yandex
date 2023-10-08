73d25e8cfeed7f116324d59418bdc628de77f3dc
Savva Siry <savvasiry@gmail.com>
2023-10-08 21:24:52 +1100

    feature(express): add express build

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./src/'));
+app.use(express.static('./dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: './dist' });
+});
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);

75722073c2389ed1d2d12ac78d2bd5e2254aebac
Savva Siry <savvasiry@gmail.com>
2023-10-08 20:55:27 +1100

    feature(pages): add profile hover

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,33 @@
-import home from './pages/home/home.hbs'
+import Handlebars from "handlebars/runtime";
+
+import myPartial from "./partials/myPartial.hbs";
+
+import chat from './pages/chat/chat.hbs';
 import login from './pages/login/login.hbs'
 import error404 from './pages/errors/error404.hbs'
 import error500 from './pages/errors/error500.hbs'
 import signin from './pages/signin/signin.hbs'
 import profile from './pages/profile/profile.hbs'
-import Handlebars from "handlebars/runtime";
-import myPartial from "./partials/myPartial.hbs"
 
+
+//Роутинг сделал функциями для передачи состояний в компоненты (на будущее)
 const ROUTES: Record<string, Function> = {
-    '/': home,
-    '/home': home,
+    '/': login,
     '/login': login,
     '/signin': signin,
     '/profile': profile,
     '/error404': error404,
     '/error500': error500,
+    '/chat': chat,
 }
 
+//Прмиер кода регистрации partials
 Handlebars.registerPartial('myPartial', myPartial);
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.querySelector('#app');
 
     if (root) {
-        console.log(root)
-        console.log(window.location.pathname)
-
         if (ROUTES[window.location.pathname])
             root.innerHTML = ROUTES[window.location.pathname]()
         else root.innerHTML = error404();

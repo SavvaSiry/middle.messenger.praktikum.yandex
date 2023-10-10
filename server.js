8fb3ff71d87b4cfe586715adca82d5a3493ec61a
Savva Siry <savvasiry@gmail.com>
2023-10-10 17:57:17 +1100

    fix(server.js): rewrite with __dirname

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: './dist' });
+app.get(['*', '/'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, function () {

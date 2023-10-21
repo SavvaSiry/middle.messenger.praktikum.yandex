de20aaf7aa109aca38aa34f41d4007adebb8c4d4
Savva Siry <savvasiry@gmail.com>
2023-10-22 04:12:39 +1100

    fix(deploy): refactor to dist

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get(['*'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
